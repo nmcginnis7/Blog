a8b2703263682c080d19c005736af92545a189f9
Nick McGinnis <nicholas.m.mcginnis@gmail.com>
2018-04-19 16:06:21 -0400

    redirect after post

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,8 +16,9 @@ export function fetchPost() {
     };
 }
 
-export function createPost(values) {
-    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+export function createPost(values, callback) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+        .then(() => callback());
 
     return {
         type: CREATE_POST,
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -23,7 +23,9 @@ class PostNew extends Component {
     }
 
     onSubmit(values) {
-        this.props.createPost(values);
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
