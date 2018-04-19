7a962b4b6c825f77860ae64b234a6477ba9f2113
Nick McGinnis <nicholas.m.mcginnis@gmail.com>
2018-04-19 14:43:25 -0400

    Added Redux Form and fields, no submit yet

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchPost } from "../actions";
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from "../actions";
+import {Link} from 'react-router-dom';
 import _ from 'lodash';
 
 class PostIndex extends Component {
-  componentDidMount() {
-    this.props.fetchPost();
-  }
+    componentDidMount() {
+        this.props.fetchPost();
+    }
 
-  renderPosts() {
-     return _.map(this.props.posts, post => {
-         return <li className="list-group-item" key={post.id}>
-             {post.title}
-         </li>
-     });
-  }
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return <li className="list-group-item" key={post.id}>
+                {post.title}
+            </li>
+        });
+    }
 
-  render () {
-    return (
-      <div>
-        <h3>Posts</h3>
-          <ul className="list-group">
-              {this.renderPosts()}
-          </ul>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {posts: state.posts}
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostIndex);
+export default connect(mapStateToProps, {fetchPost})(PostIndex);
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostNew extends Component {
-    render() {
+    renderField(field) {
         return (
-            <div>
-                PostNew
+            <div className="form-group">
+                <label>{field.label}: </label>
+                <input
+                    className="form-control"
+                    {...field.input}
+                    type="text"
+                />
             </div>
         );
     }
+
+
+
+    render() {
+        return (
+            <form>
+                <Field
+                    label="Title for Post"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
+        );
+    }
 }
-export default PostNew;
+
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostNew);
