b7fdde410a21ebc142513301e20b3804a59385fb
Nick McGinnis <nicholas.m.mcginnis@gmail.com>
2018-04-19 09:35:14 -0400

    Index Page Start

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@ export const FETCH_POSTS = 'fetch_posts'
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=tinman346';
 
+
 export function fetchPost() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from "../actions";
+import _ from 'lodash';
 
 class PostIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPost();
+  }
+
+  renderPosts() {
+     return _.map(this.props.posts, post => {
+         return <li className="list-group-item" key={post.id}>
+             {post.title}
+         </li>
+     });
+  }
+
   render () {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+          <ul className="list-group">
+              {this.renderPosts()}
+          </ul>
       </div>
     );
   }
 }
 
-export default PostIndex;
+function mapStateToProps(state){
+    return {posts: state.posts}
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostIndex);
